6ad38aab795973be5dde1f8c311d165531db38f5
DekuCool24 <dayanna.qs@gmail.com>
2020-03-13 16:43:01 -0600

    Create main.dart
    archivos importantes para el programa, no se tocan

---
+++ flutter visualizacion/proyecto principal/proyecto ejemplo/grafico burbujas/lib/main.dart
@@ -0,0 +1,43 @@
+// Copyright 2018 the Charts project authors. Please see the AUTHORS file
+// for details.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed 96on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+import 'app_config.dart';
+import 'home.dart';
+
+/// The main gallery app widget.
+class GalleryApp extends StatefulWidget {
+  GalleryApp({Key key}) : super(key: key);
+
+  @override
+  GalleryAppState createState() => new GalleryAppState();
+}
+
+/// The main gallery app state.
+///
+class GalleryAppState extends State<GalleryApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: defaultConfig.appName,
+        theme: defaultConfig.theme,
+        home: new Home());
+  }
+}
+
+void main() {
+  runApp(new GalleryApp());
+}
