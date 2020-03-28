72d282248487c0b91b6a8a217ef5b909de386388
DekuCool24 <dayanna.qs@gmail.com>
2020-03-28 14:33:12 -0600

    [New] Main
    This is the main of the application

---
+++ main.dart
@@ -0,0 +1,24 @@
+///Main
+///
+/// This is the main of the project
+///
+
+import 'package:flutter/material.dart';
+import 'Opening_Page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Animated Charts App',
+      theme: ThemeData(
+        primaryColor: Color(0xffff6101),
+      ),
+      home: HomePage(),
+    );
+  }
+}
