dfbd5370d080483dec5efbd3167766b230f723a4
DekuCool24 <dayanna.qs@gmail.com>
2020-04-20 15:51:31 -0600

    [New] Bubble_Chart\Puntarenas_Bubble_Chart\Puntarenas_Opening_Page.dart
    This is the options page for the Puntarenas bubble chart. It allows choosing what type of data to observe: Men, Women and Total Population

---
+++ Bubble_Chart/Puntarenas_Bubble_Chart/Puntarenas_Opening_Page.dart
@@ -0,0 +1,61 @@
+///Puntarenas_Opening_Page
+///
+///This page contains the main page of the project, what
+///it contains are buttons with which you can choose which
+///graphics to observe
+///
+
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'Puntarenas_Mujeres_Page.dart';
+import 'Puntarenas_Hombres_Page.dart';
+import 'Puntarenas_Poblacion_Total_Page.dart';
+
+class PuntarenasHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'Población total por condición y tipo de discapacidad, según el grupo de edad',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.lightBlue,),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.male),
+            title: Text('Hombres', style: TextStyle(fontSize: 25),),
+            onTap: () => { Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => PuntarenasHombresPage()),
+            )},
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.female),
+            title: Text('Mujeres', style: TextStyle(fontSize: 25),),
+            onTap: () => { Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => PuntarenasMujeresPage()),
+            )},
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.users),
+            title: Text('Población Total', style: TextStyle(fontSize: 25),),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PuntarenasPoblacionTotalPage()),
+              )
+            },
+          ),
+        ],
+      ),
+    );
+
+  }
+}
