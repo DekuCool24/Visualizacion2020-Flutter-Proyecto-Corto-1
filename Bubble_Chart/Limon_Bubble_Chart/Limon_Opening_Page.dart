e594edd507919bacf0028fec45818a42fa633f52
DekuCool24 <dayanna.qs@gmail.com>
2020-04-20 15:48:19 -0600

    [New] Bubble_Chart\Limon_Bubble_Chart\Limon_Opening_Page.dart
    This is the options page for the Limon bubble chart. It allows choosing what type of data to observe: Men, Women and Total Population

---
+++ Bubble_Chart/Limon_Bubble_Chart/Limon_Opening_Page.dart
@@ -0,0 +1,61 @@
+///Limon_Opening_Page
+///
+///This page contains the main page of the project, what
+///it contains are buttons with which you can choose which
+///graphics to observe
+///
+
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'Limon_Mujeres_Page.dart';
+import 'Limon_Hombres_Page.dart';
+import 'Limon_Poblacion_Total_Page.dart';
+
+class LimonHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'Población total por condición y tipo de discapacidad, según el grupo de edad',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.lime,),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.male),
+            title: Text('Hombres', style: TextStyle(fontSize: 25),),
+            onTap: () => { Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => LimonHombresPage()),
+            )},
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.female),
+            title: Text('Mujeres', style: TextStyle(fontSize: 25),),
+            onTap: () => { Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => LimonMujeresPage()),
+            )},
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.users),
+            title: Text('Población Total', style: TextStyle(fontSize: 25),),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LimonPoblacionTotalPage()),
+              )
+            },
+          ),
+        ],
+      ),
+    );
+
+  }
+}
