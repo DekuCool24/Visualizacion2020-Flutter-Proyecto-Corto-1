03b9a09aaff4a5cba6ce88709aa9b029d670b313
DekuCool24 <dayanna.qs@gmail.com>
2020-04-20 15:59:40 -0600

    [New] Parlament_Chart\alajuela_parlament_chart\alajuela_parlament_opening_page.dart
    This is the options page for the Alajuela Parlament Style chart. It allows choosing what type of data to observe: Men, Women and Total Population

---
+++ Parlament_Chart/alajuela_parlament_chart/alajuela_parlament_opening_page.dart
@@ -0,0 +1,59 @@
+///Alajuela_Opening_Page
+///
+///This page contains the main page of the project, what
+///it contains are buttons with which you can choose which
+///graphics to observe
+///
+
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'alajuela_hombres_parlament_page.dart';
+import 'alajuela_mujeres_parlament_page.dart';
+import 'alajuela_poblacion_total_parlament_page.dart';
+
+class AlajuelaParlamentHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'Población total por condición y tipo de discapacidad, según el grupo de edad',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.amberAccent,),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.male),
+            title: Text('Hombres', style: TextStyle(fontSize: 25),),
+            onTap: () => { Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AlajuelaHombresParlamentChart.withSampleData()),
+            )},
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.female),
+            title: Text('Mujeres', style: TextStyle(fontSize: 25),),
+            onTap: () => { Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AlajuelaMujeresParlamentChart.withSampleData()),
+            )},
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.users),
+            title: Text('Población Total', style: TextStyle(fontSize: 25),),
+            onTap: () => { Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AlajuelaPoblacionTotalParlamentChart.withSampleData()),
+            )},
+          ),
+        ],
+      ),
+    );
+
+  }
+}
