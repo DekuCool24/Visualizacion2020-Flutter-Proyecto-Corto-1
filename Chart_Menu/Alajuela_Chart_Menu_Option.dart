40c52dc23464776d0c03909b801e57ddf4576ee9
DekuCool24 <dayanna.qs@gmail.com>
2020-04-20 15:20:49 -0600

    [New]Chart_Menu\Alajuela_Chart_Menu_Option.dart
    This is the menu of options in the province of Alajuela. The user chooses in which type of graph he wants to see the information: Bubble, Bar or Parliamentary Style

---
+++ Chart_Menu/Alajuela_Chart_Menu_Option.dart
@@ -0,0 +1,69 @@
+///Chart_Menu_Option
+///
+///This page contains the main page of the project, what
+///it contains are buttons with which you can choose which
+///graphics to observe
+///
+
+
+import 'package:flutter/material.dart';
+import 'package:flutternuevapruebacharts/Bar_Chart/alajuela_bar_chart/alajuela_bar_opening_page.dart';
+import 'package:flutternuevapruebacharts/Bubble_Chart/Alajuela_Bubble_Chart/Alajuela_Opening_Page.dart';
+import 'package:flutternuevapruebacharts/Parlament_Chart/alajuela_parlament_chart/alajuela_parlament_opening_page.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
+class AlajuelaMenuChartOptionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'Escoja el tipo de visualización',
+              style: TextStyle(color: Colors.white, fontSize: 25, ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.teal,),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.circle),
+            title: Text('Gráfico Burbujas', style: TextStyle(fontSize: 25),
+            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AlajuelaHomePage()),
+              )
+            },
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.chartBar),
+            title: Text('Gráfico de Barra', style: TextStyle(fontSize: 25),
+            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AlajuelaBarHomePage()),
+              )
+            },
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.chartPie),
+            title: Text('Gráfico Parlamentario', style: TextStyle(fontSize: 25),
+            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AlajuelaParlamentHomePage()),
+              )
+            },
+          ),
+        ],
+      ),
+    );
+
+  }
+}
